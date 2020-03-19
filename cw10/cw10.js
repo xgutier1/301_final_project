e6b6f4704118f9ab84a86c92e3c58827334d4241
xgutier1 <xgutier1@asu.edu>
2020-03-19 16:14:58 -0700

    restructured Final-Project Directory and made template for the secondary landing pages.

---
+++ cw10/cw10.js
@@ -0,0 +1,7 @@
+var x = document.getElementsByClassName("expand_less");
+var i;
+var expandLessElements = [];
+for(i = 0; i < x.length; i++) {
+    expandLessElements.push(x[i]);
+    console.log(expandLessElements);
+}
