b3b88b901c683f0ed4e62ce630bf0da123d9e180
xgutier1 <xaviermgutierrez@gmail.com>
2020-03-19 22:18:13 -0700

    restructured Final-Project Directory and made template for the secondary landing pages.

--- cw10/cw10.js
+++ cw10/cw10.js
@@ -1,7 +1,33 @@
-var x = document.getElementsByClassName("expand_less");
-var i;
-var expandLessElements = [];
-for(i = 0; i < x.length; i++) {
-    expandLessElements.push(x[i]);
-    console.log(expandLessElements);
+const expandLessElements = document.getElementsByClassName('expand_less');
+expandLessElements[0].addEventListener('click', function() { hideDiv(this); }, false);
+expandLessElements[1].addEventListener('click', function() { hideDiv(this); }, false);
+expandLessElements[2].addEventListener('click', function() { hideDiv(this); }, false);
+expandLessElements[3].addEventListener('click', function() { hideDiv(this); }, false);
+
+const expandMoreElements = document.getElementsByClassName('expand_more');
+expandMoreElements[0].addEventListener('click', function() { showDiv(this); }, false);
+expandMoreElements[1].addEventListener('click', function() { showDiv(this); }, false);
+expandMoreElements[2].addEventListener('click', function() { showDiv(this); }, false);
+expandMoreElements[3].addEventListener('click', function() { showDiv(this); }, false);
+
+document.getElementById('numberBtn').addEventListener('click', addNumbers, false);
+
+function addNumbers() {
+    const num1 = document.getElementById('number1').value * 1;
+    const num2 = document.getElementById('number2').value * 1;
+    document.getElementById("number3").value = num1+num2;
+}
+
+function hideDiv(el) {
+    const mydiv = el.parentElement.parentElement;
+    mydiv.className = 'hidden';
+    el.nextElementSibling.style.display = '';
+    el.style.display = 'none';
+}
+
+function showDiv(el) {
+    const mydiv = el.parentElement.parentElement;
+    mydiv.className = '';
+    el.previousElementSibling.style.display = '';
+    el.style.display = 'none';
 }
